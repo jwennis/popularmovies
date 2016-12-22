53177b134d1e41df4ea1977e74b15b5a92e261c4
Joe <j.w.ennis@gmail.com>
2016-12-22 02:18:16 -0500

    initial commit

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Log.v(LOG_TAG, "Hello Popular Movies!");
     }
 }
